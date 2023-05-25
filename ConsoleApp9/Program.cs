2a94d49dba8ae829f7981a7520f4af3576013915
HBalu1026 <balazshernadi3@gmail.com>
2023-05-25 12:48:07 +0200

    szunetvege

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Linq;
 namespace ConsoleApp9
 {
     internal class Program
@@ -18,7 +18,37 @@ namespace ConsoleApp9
 
         static void Main(string[] args)
         {
+            string[] fajl = File.ReadAllLines("balkezesek.csv");
             
+            //3
+            Console.WriteLine("3.feladat: " + (fajl.Length - 1));
+
+            //4
+            Console.WriteLine("4.feladat:");
+            for (int i = 0; i < fajl.Length; i++)
+            {
+                string[] splittelt = fajl[i].Split(' ');
+                if (Convert.ToInt32(splittelt[2]) <= 1999)
+                {
+                    Console.WriteLine("\t" + splittelt[0] + ", " + (Convert.ToInt32(splittelt[4]) * 2,54) + " cm");
+                }
+            }
+
+            //5
+
+
+            //6
+            double osszes = 0;
+            for (int i = 0; i < fajl.Length; i++)
+            {
+                string[] splittelt = fajl[i].Split(' ');
+                osszes += Convert.ToInt32(splittelt[3]);
+            }
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
